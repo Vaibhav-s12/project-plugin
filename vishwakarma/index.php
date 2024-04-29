8c0e01cf88b3d78b5058f453b5160c5d5806c25e
Laxman Suthar <82657013+laxman-suthar@users.noreply.github.com>
2024-04-29 12:13:17 +0530

    4
    4

--- vishwakarma/index.php
+++ vishwakarma/index.php
@@ -25,7 +25,7 @@ class admin
 <head>
     <link href = "style.css" rel = "stylesheet">
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
